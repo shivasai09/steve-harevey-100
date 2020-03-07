df44c34d9a88849f7555821a72edd27cf2668517
v-sivsar <v-sivsar@microsoft.com>
2020-03-07 08:18:33 +0530

    initail changes

---
+++ src/Timer.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./Timer.scss";
+
+const Timer = () => {
+  return (
+    <>
+      <div className={"timerContainer"}>
+        <div className={"fill"}></div>
+      </div>
+    </>
+  );
+};
+
+export default Timer;
